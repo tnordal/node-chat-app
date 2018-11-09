6fad7484100ace68dbd9beeca9517e1901355eec
Tom Nordal <tnordal@gmail.com>
2018-11-09 21:11:34 +0100

    Created generateMessage utilit

---
+++ server/utils/message.test.js
@@ -0,0 +1,19 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+
+    it('should generate correct message object', () => {
+        var from = 'Tom';
+        var text = 'Hello';
+        var message = generateMessage(from, text);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({
+            from,
+            text
+        });
+    });
+
+});
