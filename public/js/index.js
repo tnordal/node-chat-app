05b1a9e497a2adeecc3e7d523cca8eb4620bc544
Tom Nordal <tnordal@gmail.com>
2018-11-09 18:46:29 +0100

    Add new message new user joined and welcome

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,13 @@
 var socket = io();
 
-socket.on ('connect', function () {
-    console.log('Connected to server');
-
-    // socket.emit('createMessage', {
-    //     from: 'Tom',
-    //     text: 'Hey all'
-    // });
+socket.on('connect', function () {
+  console.log('Connected to server');
 });
 
 socket.on('disconnect', function () {
-    console.log('Disconnected from server');
+  console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (message){
-    console.log('New Message', message);
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
 });
