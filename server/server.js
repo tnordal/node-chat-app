500f018031953762dda91e1352fce401b7c0c9d5
Tom Nordal <tnordal@gmail.com>
2018-11-10 22:55:51 +0100

    Add for for messages and show incomming messages

--- server/server.js
+++ server/server.js
@@ -21,16 +21,18 @@ io.on('connection', (socket) => {
   socket.broadcast.emit('newMessage', 
     generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
-    io.emit('newMessage', 
-      generateMessage(message.from, message.text));
-      
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    
+    callback('This is from the server');
+    
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // });
+
   });
 
   socket.on('disconnect', () => {
