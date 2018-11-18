4cebe310286f1986e2ed394cc11006fb5bb29e88
Tom Nordal <tnordal@gmail.com>
2018-11-18 08:45:19 +0100

    Send messages to only oeople in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -43,7 +43,7 @@ socket.on('updateUserList', function (users) {
   });
 
   jQuery('#users').html(ol);
-  
+
 });
 
 socket.on('newMessage', function (message) {
@@ -78,7 +78,6 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
